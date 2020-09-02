a1a16916fd7a4f41b509c2ef8db67b4aaaeb1784
rafael_lucio <rafaellucio.developer@gmail.com>
2020-09-02 19:54:01 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,25 @@
+var minhaPromisse = function () {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users/rafaelluciodeveloper');
+    xhr.send(null);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject('Erro na Requisição');
+        }
+      }
+    };
+  });
+};
+
+minhaPromisse()
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
